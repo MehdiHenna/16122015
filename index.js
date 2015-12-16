986bb536a6d17780a5ba8300b9417a048cd7009a
MehdiHenna <mehdihennaoui@gmail.com>
2015-12-16 14:26:03 +0100

    exercice fini et vérifier

--- index.js
+++ index.js
@@ -7,29 +7,33 @@ var me =
 	github: "https://github.com/MehdiHenna",
 	occupation: "web developeur", 
 	getFullName : function(){
-		
+		return this.name + " " + this.last_name;
 	},
 	getOccupation: function(){
-		
+		return this.occupation;
 	},
-	setName: function(){
-		
+	setName: function(n){
+		this.name = n;
+
 	},
 	getField: function(){
-		
+		return this.adresse; 
 	},
-	setField: function(){
-
+	setField: function(champ){
+		this.adresse = champ;
 	},
 	getGithub: function(){
-
+		return this.github;
 	},
-	setGithub: function(){
-
+	setGithub: function(git){
+		this.github = git;
+		
 	},
 	getInfos: function(){
-
+		return "bonjour je suis " + this.name + " " + this.last_name + " je suis né le " + this.birthey + " mon site github cest " + this.github + " mon metier c'est " + this.occupation;
 	},
 };
-
+me.setField('moi');
+console.log(me.getInfos());
+console.log(me.getField());
 console.log(me);
