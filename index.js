c6b6e0cda7ffbfc3d81537dfb887cecb08f4ad60
MehdiHenna <mehdihennaoui@gmail.com>
2015-12-16 11:51:21 +0100

    creation de l'objet me et ajout de function à l'interieur vide

---
+++ index.js
@@ -0,0 +1,35 @@
+var me = 
+{
+
+	name: "Mehdi",
+	last_name: "Hennaoui",
+	birthey: "03/01/1985",
+	github: "https://github.com/MehdiHenna",
+	occupation: "web developeur", 
+	getFullName : function(){
+		
+	},
+	getOccupation: function(){
+		
+	},
+	setName: function(){
+		
+	},
+	getField: function(){
+		
+	},
+	setField: function(){
+
+	},
+	getGithub: function(){
+
+	},
+	setGithub: function(){
+
+	},
+	getInfos: function(){
+
+	},
+};
+
+console.log(me);
