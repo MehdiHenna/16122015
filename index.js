a6ae5af58d02a2c8b55c47a5f1dbfe51ce9914a7
MehdiHenna <mehdihennaoui@gmail.com>
2015-12-16 17:04:08 +0100

    exercice réussis

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var me =
 	name: "Mehdi",
 	last_name: "Hennaoui",
 	birthey: "03/01/1985",
-	github: "https://github.com/MehdiHenna",
+	github: "MehdiHenna",
 	occupation: "web developeur", 
 	getFullName : function(){
 		return this.name + " " + this.last_name;
@@ -16,24 +16,31 @@ var me =
 		this.name = n;
 
 	},
-	getField: function(){
-		return this.adresse; 
+	getField: function(ah){
+		return this[ah]; 
 	},
-	setField: function(champ){
-		this.adresse = champ;
+	setField: function(cle, valeur){
+		this[cle] = valeur;
 	},
 	getGithub: function(){
-		return this.github;
+		return "https://github.com/" + this.github;
 	},
 	setGithub: function(git){
 		this.github = git;
 		
 	},
 	getInfos: function(){
-		return "bonjour je suis " + this.name + " " + this.last_name + " je suis né le " + this.birthey + " mon site github cest " + this.github + " mon metier c'est " + this.occupation;
+		return "bonjour je suis " + this.setName() + " je suis né le " + this.birthey + " mon site github cest " + this.github + " mon metier c'est " + this.occupation;
 	},
 };
-me.setField('moi');
-console.log(me.getInfos());
-console.log(me.getField());
-console.log(me);
+
+console.log(me.github);
+console.log(me.getGithub());
+
+
+
+
+
+
+
+
